5ebed241fd66527987256b6f164d5ac2f66d1b1b
Jhulisa Rodas Burga <117694480+JhulisaRB@users.noreply.github.com>
2024-11-01 16:26:48 -0500

    listar
    citaslist

--- frontVP/src/app/components/appointment/listarappointment/listarappointment.component.ts
+++ frontVP/src/app/components/appointment/listarappointment/listarappointment.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
+import { Appointment } from '../../../models/appointment';
+import { AppointmentService } from '../../../services/appointment.service';
 
 @Component({
   selector: 'app-listarappointment',
   standalone: true,
-  imports: [],
+  imports: [MatPaginator,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    RouterLink],
   templateUrl: './listarappointment.component.html',
   styleUrl: './listarappointment.component.css'
 })
-export class ListarappointmentComponent {
+export class ListarappointmentComponent implements OnInit {
 
+  dataSource: MatTableDataSource<Appointment> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  displayedColumns: string[] = [
+    'codigo',
+    'nombre',
+    'fecha',
+    'estado',
+    'veterano',
+    'psicologo',
+    'accion01',
+    'accion02',
+  ];
+  constructor(private aS: AppointmentService) {}
+
+  ngOnInit(): void {
+    this.aS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.aS.getList().subscribe((data) => { //rediccionar
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
+  eliminar(id:number){
+    this.aS.delete(id).subscribe((data)=>{
+      this.aS.list().subscribe((data)=>{
+        this.aS.setList(data)
+      })
+    })
+  }
 }
--- frontVP/src/app/services/user.service.ts
+++ frontVP/src/app/services/user.service.ts
@@ -36,4 +36,7 @@ export class UserService {
   delete(id: number) {
     return this.http.delete(`${this.url}/${id}`);
   }
+  listarPorRol(rol:string) {
+    return this.http.get<User[]>(`${this.url}/listarrol/${rol}`);
+  }
 }
