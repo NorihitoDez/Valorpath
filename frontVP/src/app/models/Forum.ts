4c45a3fb90ece6a5eb7859566fbe5e7dfdf0c6f1
Choxclin <108918759+Choxclin@users.noreply.github.com>
2024-11-06 19:20:06 -0500

    models->Forum.ts

---
+++ frontVP/src/app/models/Forum.ts
@@ -0,0 +1,11 @@
+import { User } from "./user"
+
+export class Forum{
+    id:number=0
+    title:string=""
+    description:string=""
+    date:Date=new Date(Date.now())
+    // Relación con la taba Users [psiciologo
+    psychologist:User=new User()
+
+}
